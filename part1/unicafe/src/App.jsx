796f2928df4bbd8111b06b309e34860962ed06c6
vedant jangid <vedjangid341@gmail.com>
2023-09-14 23:34:27 +0530

    unicafe completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,62 +1,124 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const StatisticLine = (props) => {
-  return props.text == "positive" ? (
-    <>
-      <p>
-        {props.text} {props.value}%
-      </p>
-    </>
-  ) : (
-    <p>
-      {props.text} {props.value}
-    </p>
+const Heading = (props) => <h1>{props.text}</h1>;
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+
+const Statistic = (props) => {
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.value}</td>
+    </tr>
   );
 };
 
 const Statistics = (props) => {
-  return props.total > 0 ? (
-    <>
-      <h2>statistics</h2>
-      {/* <p>positive {props.positive ? props.positive : 0}%</p> */}
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.total} />
-      <StatisticLine text="average" value={props.total / 3} />
-      <StatisticLine text="positive" value={props.positive} />
-    </>
-  ) : (
-    <>
-      <h2>statistics</h2>
-      <h4>No feedback given</h4>
-    </>
+  if (!props.hasFeedback) {
+    return <p>No feedback has been given</p>;
+  }
+
+  return (
+    <div>
+      <Heading text="Statistics" />
+      <table>
+        <tbody>
+          <Statistic name="good" value={props.good} />
+
+          <Statistic name="neutral" value={props.neutral} />
+
+          <Statistic name="bad" value={props.bad} />
+
+          <Statistic name="total" value={props.total} />
+
+          <Statistic name="averageScore" value={props.averageScore} />
+
+          <Statistic
+            name="percentPositive"
+            value={props.percentPositive + "%"}
+          />
+        </tbody>
+      </table>
+    </div>
   );
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [hasFeedback, setHasFeedback] = useState(false);
 
-  let total = good + bad + neutral;
-  let pos = (good / total) * 100;
+  const total = good + neutral + bad;
+
+  const getPercent = (x, totalAmount) => {
+    let result = (x / totalAmount) * 100;
+
+    if (Number.isNaN(result)) return 0;
+
+    return Math.round(result * 1000) / 1000;
+  };
+
+  const percentPositive = getPercent(good, total);
+
+  const getWeightedAvg = (weightsArr, total) => {
+    const weights = weightsArr.reduce((acc, item) => {
+      return acc + item.number * item.weight;
+    }, 0);
+
+    let result = weights / total;
+
+    if (Number.isNaN(result)) return 0;
+
+    return Math.round(result * 1000) / 1000;
+  };
+
+  const averageScore = getWeightedAvg(
+    [
+      { number: good, weight: 1 },
+      { number: neutral, weight: 0 },
+      { number: bad, weight: -1 },
+    ],
+    total
+  );
+
+  const handleButtonClick = (type) => {
+    setHasFeedback(true);
+
+    switch (type) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const statisticsProps = {
+    hasFeedback: hasFeedback,
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+    averageScore: averageScore,
+    percentPositive: percentPositive,
+  };
 
   return (
-    <>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics
-        good={good}
-        bad={bad}
-        neutral={neutral}
-        total={total}
-        positive={pos}
-      />
-    </>
+    <div>
+      <Heading text="Give Feedback" />
+      <Button onClick={() => handleButtonClick("good")} text="good" />
+      <Button onClick={() => handleButtonClick("neutral")} text="neutral" />
+      <Button onClick={() => handleButtonClick("bad")} text="bad" />
+      <Statistics {...statisticsProps} />
+    </div>
   );
 };
 
